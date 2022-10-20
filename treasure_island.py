a453fcc84db800855867834d47818e984cc4973d
aclcoelho <ana.lunardello@gmail.com>
2022-10-20 01:37:34 -0300

    Altering first_option

--- treasure_island.py
+++ treasure_island.py
@@ -24,7 +24,7 @@ print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
 
 first_option = input("You're at a cross road. Where do you want to go? Type 'left' or 'right'\n").lower()
-if first_option == "left" or first_option == "Left":
+if first_option == "left":
     second_option = input("You come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across.\n").lower()
     if second_option == "wait":
         third_option = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you choose?\n").lower()
